de1453f9216bc6122b90ed3bb5f51672d52fd3f6
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-14 20:07:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,9 @@ def line(current_number)
   if current_number = 0 
     puts "The line is currently empty."
   else 
+  new_line = []
   i = 0 
+ newer = "#{i +1}. #{current_number}"
   
   #{current_number}"
   end
